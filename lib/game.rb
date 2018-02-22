e6f6c32559fd2913a3464d32925881060d278c84
shen <shensat18@gmail.com>
2018-02-22 12:44:20 +0000

    git init and first couple of tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,25 @@
+class Game
+	attr_accessor :frames, :total
 
+	def initialize
+		@rolls = []
+		@frames = []
+		@total = 0
+	end
+
+
+	def roll(pins)
+		@rolls.push(pins) 
+	end
+
+	def make_frames
+		@rolls.each_with_index { |roll, i| @frames.push([roll, @rolls[i+1]]) unless (i+1).even? }
+	end
+
+	def sum
+		@frames.each do |frame|
+			@total += frame.reduce(:+)
+		end
+		#@rolls.reduce(:+)
+	end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,20 @@
+require_relative '../lib/game'
 
+describe 'Game' do 
+	it 'should calculate the score for 20 rolls of 1 pin each as 20' do 
+		game = Game.new
+		20.times {game.roll(1)}
+		game.make_frames
+		game.sum
+		expect(game.total).to eq(20)
+	end
+
+	it 'should package rolls into frames' do 
+		game = Game.new
+		4.times {game.roll(1)}
+		game.make_frames
+		expect(game.frames).to eq([[1,1],[1,1]])
+	end
+
+	
+end
